9d2562410292101c71d4233b1c46e46642af1397
jensaly <jens.lyngaas@gmail.com>
2023-06-15 21:17:31 +0200

    Replaced meshgrid solution with built-in Julia stuff.

--- kuramotoNO.jl
+++ kuramotoNO.jl
@@ -6,8 +6,8 @@ Pkg.add("Plots")
 =#
 
 using DifferentialEquations
-using StaticArrays
-using LinearAlgebra
+#using StaticArrays
+#using LinearAlgebra
 
 
 function meshgrid(x, y)
@@ -16,6 +16,20 @@ function meshgrid(x, y)
     X, Y
  end
 
+ 
+function kuramotoNO!(du, u, p, t)
+    ω, K, N = p
+    #display(u)
+    θi = reduce(hcat, repeat([u], N, 1))
+    θj = transpose(θi)
+    phase_diff = sin.(θj - θi)
+    summed = sum(phase_diff, dims = 2)
+    for i = 1:N
+        du[i] = ω[i] + K * summed[i]
+    end
+end
+
+#=
 function kuramotoNO!(du, u, p, t)
     ω, K, N = p
     θi, θj = meshgrid(u,u)
@@ -25,16 +39,18 @@ function kuramotoNO!(du, u, p, t)
         du[i] = ω[i] + K * summed[i]
     end
 end
+=#
 
 # Define initial conditions and parameters
 u0 = [0.0, 1.0, 2.0]   # Initial phase values
+ω = [6.6e9, 6.7e9, 6.2e9]
 K = 0.3e9
-N = 3
-p = [[6.6e9, 6.7e9, 6.2e9], K, N]    # Natural frequencies
+N = length(u0)
+p = [ω, K, N]    # Natural frequencies
 
 
 tstart = 0.0     # Start time
-tend = 1000e-9      # End time
+tend = 100e-9      # End time
 dt = 1e-12        # Time step
 
 tspan = (tstart, tend)  # Time span for simulation
@@ -49,10 +65,12 @@ t = range(tspan[1], tspan[2], length=1000)
 θ1 = [sol(ti)[1] for ti in t]
 θ2 = [sol(ti)[2] for ti in t]
 θ3 = [sol(ti)[3] for ti in t]
+#θ4 = [sol(ti)[4] for ti in t]
 
 f1 = diff(θ1) / (t[2] - t[1]) / 1e9
 f2 = diff(θ2) / (t[2] - t[1]) / 1e9
 f3 = diff(θ3) / (t[2] - t[1]) / 1e9
+#f4 = diff(θ4) / (t[2] - t[1]) / 1e9
 
 # Plot the results
 using Plots
@@ -64,5 +82,6 @@ ylabel!("Phase differnece")
 plot(f1, label="Oscillator 1")
 plot!(f2, label="Oscillator 2")
 plot!(f3, label="Oscillator 3")
+#plot!(f4, label="Oscillator 4")
 xlabel!("Time")
 ylabel!("Phase differnece")
--- testingstuff.jl
+++ testingstuff.jl
@@ -1,8 +1,18 @@
 using DifferentialEquations
-f(u,p,t) = 0.98u
-u0 = 1.0
-tspan = (0.0,1.0)
-prob = ODEProblem(f,u0,tspan)
-solve(prob)
+
+function vector_to_row_matrix(vector::Vector{T}, n::Int) where T
+    matrix = reduce(hcat, repeat([vector], n, 1))
+    return matrix
+end
+
+
+vector = [1, 2, 3, 4, 5]
+n = 5
+matrix1 = vector_to_row_matrix(vector, n)
+matrix2 = transpose(vector_to_row_matrix(vector, n))
+
+display(vector)
+display(matrix1 - matrix2)
+#display(matrix2)
 
 print("Exit code 0")
