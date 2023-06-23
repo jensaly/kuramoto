9e6db405e90b1830364828b7912f5bbec9b8ba3e
jensaly <jens.lyngaas@gmail.com>
2023-06-23 19:12:11 +0200

    Added a repeat of Garg et al, showing the synchronization map is closely replicated. Good.

--- examples/garg et al/garg.jl
+++ examples/garg et al/garg.jl
@@ -22,18 +22,21 @@ function output_freq_diff(model::Kuramoto)
 end
 
 N = 4
-K = 0.97e9
+K = 0.05e9
 
 u = zeros(4)
 
-ω = [6.6e9, 6.7e9, 6.2e9, 6.4e9]
+ω = [0,0,0,0] # Placeholder
 K = create_standard_K(K, N)
-#=
-K[1,2] /= 3
-K[2,1] /= 3
-K[3,4] /= 3
-K[4,3] /= 3
-=#
+
+K[1,2] = 0.0398e9 / 4
+K[2,1] = 0.0398e9 / 4
+K[3,4] = 0.0398e9 / 4
+K[4,3] = 0.0398e9 / 4
+
+K = 4 * K
+display(K)
+
 tstart = 0.0     # Start time
 tend = 100e-9      # End time
 dt = 1e-12        # Time step
@@ -41,43 +44,21 @@ dt = 1e-12        # Time step
 tspan = (tstart, tend)  # Time span for simulation
 model = Kuramoto(u, ω, K, tstart, tend, dt)
 
-#display(K)
-
 freq_diff = zeros(300,300)
-natfreq1 = range(96e9, 104e9, 300)
-natfreq2 = range(96e9, 104e9, 300)
-
-#display(natfreq1)
-
+natfreq1 = range(6.20e9, 7.20e9, 300)
+natfreq2 = range(6.20e9, 7.20e9, 300)
 
 for i in 1:length(natfreq1)
     for j in 1:length(natfreq2)
-        model.ω = [natfreq1[i], natfreq2[j], 99.5e9, 100.5e9]
+        model.ω = [natfreq1[i], natfreq2[j], 6.63e9, 6.81e9]
         run_kuramoto(model, 1e-12, 1e-12)
         freq_diff[i,j] = output_freq_diff(model)
-        #print(model.ω, "\n")
     end
     print(i)
 end
 
-filename = "examples/syncronization map/matrix_output.txt"
+filename = "examples/garg et al/garg.txt"
 
 writedlm(filename, freq_diff, '\t')
 print("Exit code 0")
-#=
-run_kuramoto(model)
 
-plot_model_frequencies(model)
-
-model.ω = [6.6e9, 6.7e9, 6.2e9, 4.4e9]
-
-run_kuramoto(model)
-
-plot_model_frequencies(model)
-
-model.ω = [6.6e9, 6.7e9, 6.2e9, 2.4e9]
-
-run_kuramoto(model)
-
-plot_model_frequencies(model)
-=#
